204e18989f866e7e4d32a9940306c0fad2c673cd
Gochi <mevere66@gmail.com>
2022-02-11 15:50:20 -0500

    Se agregan los ejercicios del 7 al 10

---
+++ src/clases/Ejercicio7.java
@@ -0,0 +1,22 @@
+package clases;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    /*
+    Lee un número por teclado y comprueba que este numero es mayor
+    o igual que cero, si no lo es lo volverá a pedir (do while),
+    después muestra ese número por consola.
+*/
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        double numero;
+        do {
+            System.out.println("Ingrese numero");
+            numero = leer.nextDouble();
+
+        } while (numero <= 0);
+        System.out.println("El numero es: "+ numero);
+    }
+
+}
