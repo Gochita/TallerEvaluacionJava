d9d7254ba224aabb1028ae755d493c79f1a0c86b
Gochi <mevere66@gmail.com>
2022-02-11 03:10:49 -0500

    Se agregan los ejercicios 4 y 5

---
+++ src/clases/Ejercicio4.java
@@ -0,0 +1,21 @@
+package clases;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    private static final double iva=21;
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el valor del producto");
+        double cantidad= leer.nextDouble();
+        double valor=calcularIva(cantidad);
+        double precioFinal= cantidad+valor;
+        System.out.println("El precio final con iva es "+ precioFinal);
+
+
+    }
+    private static double calcularIva(double cantidad){
+        double resultado= (iva/100)*cantidad;
+        return resultado;
+    }
+}
---
+++ src/clases/Ejercicio5.java
@@ -0,0 +1,32 @@
+package clases;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int cont = 0;
+        int numero = 1;
+        System.out.println("Los numeros impares son:");
+        while (cont < 100) {
+            if (numero % 2 != 0) {
+                System.out.println(numero);
+
+                cont++;
+            } else {
+                cont++;
+            }
+            numero++;
+        }
+        cont=0;
+        numero=1;
+        System.out.println("Los numeros pares son: ");
+        while (cont < 100) {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+
+                cont++;
+            } else {
+                cont++;
+            }
+            numero++;
+        }
+    }
+}
